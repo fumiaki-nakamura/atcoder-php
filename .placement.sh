99351fb352b2a947b0344040a147dd06d5924f24
f.nakamura <f.nakamura@libry.jp>
2020-04-25 15:23:39 +0900

    laravel_template: .placement.sh 修正

--- .placement.sh
+++ .placement.sh
@@ -1,26 +1,25 @@
 #!/bin/sh
 
 cd `dirname $0`
-currentPath=`dirname $0`
-exclusionFiles=`\find $currentPath -maxdepth 1 -type f`
-laravelPath=$currentPath/.laravel
-files=`\find $laravelPath -maxdepth 1 -type f`
+currentPath=`pwd`
+exclusionFiles=`find $currentPath -maxdepth 1 -type f`
 
 # 前回スクリプトで実行した際に展開していたシンボリックリンクを一旦削除
 # ※ ファイルパスの変更があった場合に `composer dump-autoload` が失敗するのを防ぐため
 find . -type l | grep -v '/vendor/' | xargs rm -f
 
-cp -R .laravel/* $currentPath
-for file in `\find .customize -maxdepth 1 -type f`;
-do
-    ln -nfs $file
-done
+# Laravel の初期セットを展開
+find .laravel -mindepth 1 -maxdepth 1 | xargs cp -at .
 
-# ./src 配下のファイルを ./ にシンボリックリンクとして展開
-for path in `find src/ -type f`
+# laravel_template の初期セットを展開
+find .customize -mindepth 1 -maxdepth 1 | xargs cp -at .
+
+# src 配下のファイルを ./ にシンボリックリンクとして展開
+mkdir -p src
+for path in `find src -type f`
 do
-    relativePath=`echo ${path#src/*/} | sed -Ee 's;(/?)[^/]+;\1..;g'`/$path
-    linkPath=${path#src/*}
-    mkdir -p `dirname $linkPath`
-    ln -nfs $relativePath $linkPath
+    dst=${path#src/}
+    src=`echo $dst | sed -Ee 's;[^/]+/;../;g' | sed -Ee "s;${path##*/};$path;"`
+    mkdir -p `dirname $dst`
+    ln -nfs $src $dst
 done
