f54a30a1315ef5d2b5d2eb2b23a001a677811082
f.nakamura <f.nakamura@libry.jp>
2020-05-13 03:49:23 +0900

    1 day commit

--- src/resources/views/at_coder.blade.php
+++ src/resources/views/at_coder.blade.php
@@ -13,6 +13,12 @@
             background: #ddd;
             overflow: hidden;
         }
+        textarea {
+            display: block;
+            width: 100%;
+            height: 100%;
+            background: #def;
+        }
         button {
             border-radius: 6px;
             line-height: 1.5em;
@@ -27,8 +33,15 @@
             padding-left: 3px;
             overflow: hidden;
         }
+        #codeList {
+            min-width: 16em;
+        }
+        #codeName {
+            width: 16em;
+        }
         .grid-main {
             grid-area: main;
+            position: relative;
             min-width: 300px;
             max-width: calc(100vw - 300px);
             width: 50vw;
@@ -42,6 +55,12 @@
         }
         .inline-box {
             display: inline-block;
+            height: 100%;
+        }
+        .inline-box+.inline-box {
+            margin-left: 5px;
+            border-left: 1px solid #ccc;
+            padding-left: 10px;
         }
         .box-label {
             position: relative;
@@ -80,27 +99,31 @@
             height: 300px;
             resize: vertical;
         }
-        .delete-button {
+        .float-button {
             z-index: 1;
             position: absolute;
             right: 5px;
+            margin-top: 5px;
+            width: 2em;
+            padding: 1px 0;
+        }
+        #copySubmitCode {
+            width: 11em;
         }
         .result {
             box-sizing: border-box;
-            width: 100%;
             resize: vertical;
-            background: #def;
         }
-        .result.passed {
+        .result.app-passed {
             background: #bfd;
         }
-        .result.failed {
+        .result.app-failed {
             background: #fdd;
         }
         .check-label {
             display: block;
         }
-        .loading {
+        .app-loading {
             position: relative;
         }
         .loading-filter {
@@ -112,7 +135,7 @@
             height: 100%;
             background: rgba(0, 0, 0, 0.25);
         }
-        .loading>.loading-filter {
+        .app-loading>.loading-filter {
             display: block;
         }
         .loading-position {
@@ -169,17 +192,25 @@
 </head>
 <body class="grid">
 <div class="grid-header">
-    <div class="inline-box"><button type="button" id="saveCodes">Save codes (⌘+S)</button></div>
-    <div class="inline-box"><button type="button" id="addTestCase">Add a test case</button></div>
+    <div class="inline-box">
+        <select id="codeList" title="codeList">
+            <option value="">(new)</option>
+        </select>
+        <button type="button" id="openCodes">Open codes (⌘+O)</button>
+    </div>
+    <div class="inline-box">
+        <input type="text" id="codeName" title="code name" placeholder="code name">
+        <button type="button" id="saveCodes">Save codes (⌘+S)</button>
+    </div>
     <div class="inline-box"><button type="button" id="runTestCases">Run test cases (⌘+R)</button></div>
 </div>
 <div class="grid-main">
-    <div class="box"><button type="button" id="copySubmitCode">Copy a submit code to the clipboard</button></div>
+    <button type="button" class="float-button" id="copySubmitCode">Copy a submit code</button>
     <label class="box-label" for="mainEditor">Main Code</label>
     <div class="box editor-box">
         <textarea id="mainEditor">{{file_get_contents(resource_path('assets/php/main_editor.php'))}}</textarea>
     </div>
-    <label class="box-label" for="devEditor">Additional Code for Self-Check</label>
+    <label class="box-label" for="devEditor">Additional Code for Test</label>
     <div class="box editor-box">
         <textarea id="devEditor">{{file_get_contents(resource_path('assets/php/dev_editor.php'))}}</textarea>
     </div>
@@ -191,8 +222,9 @@
 </div>
 <div id="testCaseBox" class="grid-sub">
     <div>
+        <button type="button" class="float-button" id="addTestCase">＋</button>
         <label class="box-label app-close" for="testCases">Test Cases</label>
-        <div id="testCases">
+        <div class="box" id="testCases">
             <label class="check-label"><input type="checkbox" id="bailOut" checked><small>Stop immediately if one test case fails.</small></label>
             <label class="check-label"><input type="checkbox" id="onlyFail" checked><small>Run the only failed test cases.</small></label>
             <label class="check-label">Time Limit: <input type="number" id="timeLimit" min="1" max="60" value="2"><small>seconds</small></label>
@@ -200,16 +232,16 @@
         </div>
     </div>
     {{-- dummy --}}
-    <div style="display: none;" class="loading">
+    <div style="display: none;" class="app-loading">
         <div class="loading-filter"><div class="loading-position"><div class="loading-animation"></div></div></div>
-        <button class="delete-button">−</button>
+        <button class="float-button delete-button">−</button>
         <label class="box-label app-close" for="testCaseEditor0">Test Case 0</label>
         <div class="box editor-box">
-            <textarea id="testCaseEditor0"></textarea>
+            <textarea id="testCaseEditor0" readonly></textarea>
         </div>
         <label class="box-label" for="result0">Test Result 0</label>
         <div class="box">
-            <textarea class="result passed failed" id="result0" readonly></textarea>
+            <textarea class="result app-passed app-failed" id="result0" readonly></textarea>
         </div>
     </div>
 </div>
@@ -241,7 +273,6 @@ function submit() {
     const testCases = [];
     const mainCode = mainEditor.doc.getValue();
     const devCode = devEditor.doc.getValue();
-    const prdCode = prdEditor.doc.getValue();
     const bailOut = document.getElementById("bailOut").checked;
     const onlyFail = document.getElementById("onlyFail").checked;
     const timeLimit = +document.getElementById("timeLimit").value;
@@ -251,41 +282,20 @@ function submit() {
     for (const id in testCaseSet) {
         const testCase = testCaseSet[id];
 
-        testCases.push(testCase);
-
-        if (onlyFail && testCase.passed) {
+        if (onlyFail && testCase.isPassed()) {
             continue;
         }
 
+        testCases.push(testCase);
         testCase.startLoading();
     }
 
-    runTestCodes()
-        .then(() => {
-            console.log("end", {
-                main_code: mainCode,
-                dev_code: devCode,
-                prd_code: prdCode,
-                test_codes: testCases.map(testCase => testCase.getCode()),
-                bail_out: bailOut,
-                only_fail: onlyFail,
-                time_limit: timeLimit,
-                memory_limit: memoryLimit,
-            });
-        });
-
-    function runTestCodes() {
-        return next(0);
-    }
+    next(0);
 
     function next(i) {
         const testCase = testCases[i];
         if (!testCase) {
             return Promise.resolve();
-        }
-
-        if (onlyFail && testCase.passed) {
-            return next(i + 1);
         }
 
         if (bailOut && failedOnce) {
@@ -299,7 +309,7 @@ function submit() {
             .then(() => {
                 testCase.finishLoading();
 
-                if (!testCase.passed) {
+                if (!testCase.isPassed()) {
                     failedOnce = true;
                 }
 
@@ -365,7 +375,7 @@ class TestCase {
 
         element.insertAdjacentHTML("beforeend", `<hr>
 <div class="loading-filter"><div class="loading-position"><div class="loading-animation"></div></div></div>
-<button class="delete-button">−</button>
+<button class="float-button delete-button">−</button>
 <label class="box-label" for="testCaseEditor${id}">Test Case ${id}</label>
 <div class="box editor-box">
     <textarea id="testCaseEditor${id}" readonly>{{file_get_contents(resource_path('assets/php/case_editor.php'))}}</textarea>
@@ -378,14 +388,32 @@ class TestCase {
         );
         document.getElementById("testCaseBox").appendChild(element);
 
+        const drop = () => {
+            resizeObserver.unobserve(this.element.querySelector(".editor-box"));
+            delete testCaseSet[this.id];
+
+            this.editor.toTextArea();
+            this.editor = null;
+
+            const deleteButton = this.element.querySelector(".delete-button");
+            deleteButton.removeEventListener("click", drop, false);
+            deleteButton.remove();
+
+            this.element.querySelectorAll(".box-label").forEach(element => element.classList.add("app-close"));
+
+            this.element = null;
+        };
+
+        element.querySelector(".delete-button").addEventListener("click", drop, false);
         element.querySelectorAll(".box-label").forEach(
             element => element.addEventListener("click", event => event.target.classList.toggle("app-close"), false)
         );
 
-        this.id      = id;
-        this.element = element;
-        this.editor  = createEditor(`testCaseEditor${id}`);
-        this.passed  = false;
+        this.id         = id;
+        this.element    = element;
+        this.editor     = createEditor(`testCaseEditor${id}`);
+        this.passed     = false;
+        this.testedCode = null;
 
         testCaseSet[id] = this;
         resizeObserver.observe(element.querySelector(".editor-box"));
@@ -399,36 +427,30 @@ class TestCase {
         return this.editor.doc.getValue();
     }
 
+    isPassed() {
+        return this.passed && this.getCode() === this.testedCode;
+    }
+
     startLoading() {
-        this.element.classList.add("loading");
+        this.element.classList.add("app-loading");
     }
 
     finishLoading() {
-        this.element.classList.remove("loading");
+        this.element.classList.remove("app-loading");
     }
 
     setResult(result) {
         const element = this.element.querySelector(".result");
         element.value = result.message;
-        element.classList.remove("passed", "failed");
+        element.classList.remove("app-passed", "app-failed");
         if (result.passed === true) {
-            element.classList.add("passed");
+            element.classList.add("app-passed");
         } else if (result.passed === false) {
-            element.classList.add("failed");
+            element.classList.add("app-failed");
         }
 
-        this.passed = result.passed || false;
-    }
-
-    drop() {
-        resizeObserver.unobserve(this.element.querySelector(".editor-box"));
-        delete testCaseSet[this.id];
-
-        this.editor.toTextArea();
-        this.editor  = null;
-
-        this.element.remove();
-        this.element = null;
+        this.passed     = result.passed || false;
+        this.testedCode = this.getCode();
     }
 }
 
@@ -489,8 +511,5 @@ TestCase.create();
 </body>
 </html>
 {{--
-TODO: delete (-)
-TODO: add (+)
-TODO: failed/changed test cases
 TODO: save & load codes
 --}}
